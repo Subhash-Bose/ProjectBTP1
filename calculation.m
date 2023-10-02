b4389c8d5415745be85f36356987fc410ddbbfb1
CoolBose <CoolBose@10.38.2.159>
2023-10-02 21:40:14 +0530

    Major changes in Model.slx and setup_environment

--- calculation.m
+++ calculation.m
@@ -1,8 +1,22 @@
 
-
-
-If_pre_0=out.Ia_0.signals.values(26,1);
-If_post_0=out.Ia_0.signals.values(27,1);
+If_pre_0=[out.Ia_0.signals.values(26,1) out.Ib_0.signals.values(26,1) out.Ic_0.signals.values(26,1)]';
+If_post_0=[out.Ia_0.signals.values(27,1) out.Ib_0.signals.values(27,1) out.Ic_0.signals.values(27,1)]';
 
 If_pure_0=If_post_0-If_pre_0;
+
+Z_0S=Z_S;
+
+Z_SR=Z_01;
+Z_1R=Z_12+Z_23;
+
+m=0.2;
+for k=1:5
+
+    Dm=(Z_0S+Z_SR+Z_1R)/((1-m)*Z_SR+Z_1R);
+
+    If=Dm*If_pure_0;
+
+    m=imag((conj(If)')*V_S)./imag((conj(If)')*Z_SR*If_post_0)
+
+end
 
--- setup_environment.m
+++ setup_environment.m
@@ -1,61 +1,71 @@
 clear all;
 close all;
 
+alpha=cosd(120)+1i*sind(120);
+
 % Source Modelling
-V_S=11000;                  %Ph-Ph voltage
+Va_S=11e3;                  %Ph-Ph voltage
 f=50;                       %frequency in Hz
-R_S=0.8913;
-L_S=16E-3;
-Z_S=R_S+j*2*pi*f*L_S;
+S_S=58e6;
+XR_S=10;
 
+z_s=0.2076+1i*2.0759;
+Z_S=diag([z_s z_s z_s]);
+V_S=[Va_S Va_S.*alpha Va_S.*alpha.^2]';
 
+% Line Modelling
 
-%Line Modelling
-r=0.4013;                   %Ohms resistance per Km
-x=1.413;                   %Ohms reactance per Km
-l=x./(2*pi*f);              %Inductance in H per Km
+z=[[0.4013+1i*1.4133 0.0953+1i*0.8515 0.0953+1i*0.7266]
+   [0.0953+1i*0.8515 0.4013+1i*1.4133 0.0953+1i*0.7802]
+   [0.0953+1i*0.7266 0.0953+1i*0.7802 0.4013+1i*1.4133]];
 
-%Modelling 0-1 Feeder line
-l1=3;                       %line length in Km
-R_01=r.*l1;                 %Line resistance in Ohms
-L_01=l.*l1;                 %Line inductance in H
-Z_01=R_01+j*2*pi*f*L_01;
+%Line 0-1
+l1=3;
+Z_01=z*l1;
+R_01=real(Z_01);
+X_01=imag(Z_01);
+L_01=X_01/(2*pi*f);
 
-%Modelling 1-2 Feeder line
-l2=3.5;                     %line length in Km
-R_12=r.*l2;                 %Line resistance in Ohms
-L_12=l.*l2;                 %Line inductance in H
-Z_12=R_12+j*2*pi*f*L_12;
+% Line 1-2
+l2=3.5;
+Z_12=z*l2;
+R_12=real(Z_12);
+X_12=imag(Z_12);
+L_12=X_12/(2*pi*f);
 
-%Modelling 2-3 Feeder line
-l3=1.8;                     %line length in Km
-R_23=r.*l3;                 %Line resistance in Ohms
-L_23=l.*l3;                 %Line inductance in H
-Z_23=R_23+j*2*pi*f*L_23;
+% Line 2-3
+l3=1.8;
+Z_23=z*l3;
+R_23=real(Z_23);
+X_23=imag(Z_23);
+L_23=X_23/(2*pi*f);
 
 %Load L1 Modelling
-V_L1=V_S;                   %Line Voltage
+Va_L1=Va_S;                   %Line Voltage
 P_L1=100000;                %Real Power
 pf_L1=0.9;                  %Load power factor
 Q_L1=reactive(P_L1,pf_L1);  %Reactive Power
 
 %Load L2 Modelling
-V_L2=V_S;                   %Line Voltage
+Va_L2=Va_S;                   %Line Voltage
 P_L2=50000;                 %Real Power
 pf_L2=0.9;                  %Load power factor
 Q_L2=reactive(P_L2,pf_L2);  %Reactive Power
 
 %Load L3 Modelling
-V_L3=V_S;                   %Line Voltage
+Va_L3=Va_S;                   %Line Voltage
 P_L3=80000;                 %Real Power
 pf_L3=0.9;                  %Load power factor
 Q_L3=reactive(P_L3,pf_L3);  %Reactive Power
 
 
-% Fault Modelling
+%Fault Modeling
 m_actual=0.2;
-R_01_1=r.*l1.*m_actual;     %Line resistance in Ohms
-L_01_1=l.*l1.*m_actual;     %Line inductance in H
+l1_1=m_actual*l1;
+R_011=m_actual*R_01;
+L_011=m_actual*L_01;
 
-R_01_2=r.*l1.*(1-m_actual); %Line resistance in Ohms
-L_01_2=l.*l1.*(1-m_actual); %Line inductance in H
+l1_2=(1-m_actual)*l1;
+R_012=(1-m_actual)*R_01;
+L_012=(1-m_actual)*L_01;
+
